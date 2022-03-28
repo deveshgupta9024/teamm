6a774ca5a7cd670421f0d393d4b14d9e14d9441e
G8-2110990159 <amarbir0159.be21@chitkara.edu.in>
2022-03-28 11:29:55 +0530

    fourth commit

--- hello.cpp
+++ hello.cpp
@@ -11,3 +11,25 @@ int main()
 {
 	cout<<"hi third text"<<endl;
 }
+
+#include<iostream>
+using namespace std;
+int main(){
+int arr[5][2] = {{2,2},
+                 {5,8},
+                 {4,3},
+                 {2,2},
+                 {4,3}
+                 };
+
+
+for(int i=0;i<5;i++)
+{
+    for(int j=0;j<2;j++)
+    {
+        cout<<arr[i][j]<<" ";
+    }
+    cout<<"\n";
+}
+return 0;
+}
